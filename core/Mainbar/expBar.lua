1f908d20559b4ab80fc5e2c251cb1f8c68733b59
Nils89 <nils-reinecke@hotmail.de>
2026-03-21 07:59:51 +0100

    fix #260

--- core/Mainbar/expBar.lua
+++ core/Mainbar/expBar.lua
@@ -424,7 +424,7 @@ local function UpdateData(self)
             end
         end
 
-        if not GW.Retail and select(2, HasPetUI()) and UnitLevel("pet") < level then
+        if not GW.Retail and select(2, HasPetUI()) and (UnitLevel("pet") or 0) < level then
             showPet, valPrecPet = UpdatePetXPClassic(self, level)
         end
 
