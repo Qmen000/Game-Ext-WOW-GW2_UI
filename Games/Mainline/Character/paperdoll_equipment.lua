d9dabfc290ac1638c0a0cc4803bed5e4023ed47e
Nils89 <nils-reinecke@hotmail.de>
2026-04-13 09:44:51 +0200

    typo

--- Games/Mainline/Character/paperdoll_equipment.lua
+++ Games/Mainline/Character/paperdoll_equipment.lua
@@ -621,7 +621,7 @@ end
 
 local function updateUnitData(self)
     self.characterName:SetText(UnitPVPName("player"))
-    local name = select(2, C_SpecializationInfo.GetSpecializationInfo(GW.myspec, false, false, false, GW.mysex))
+    local name = select(2, C_SpecializationInfo.GetSpecializationInfo(GW.myspec, false, false, nil, GW.mysex))
 
     if name ~= nil then
         local data = LEVEL .. " " .. GW.mylevel .. " " .. name .. " " .. GW.myLocalizedClass
