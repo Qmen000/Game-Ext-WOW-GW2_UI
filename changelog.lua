047660828a15f86898b9b9061d4e711c76884640
Nils89 <nils-reinecke@hotmail.de>
2026-08-24 07:53:24 +0200

    Fix actionbar binding error on era clients

--- changelog.lua
+++ changelog.lua
@@ -13,6 +13,10 @@ AddChange(string addonVersion, table changeList)
   }
 ]]
 
+addChange("11.1.6", {
+    {GW.Enum.ChangelogType.bug, [=[Fix actionbar binding error on era clients]=]},
+})
+
 addChange("11.1.5", {
     {GW.Enum.ChangelogType.bug, [=[Fix evoker ebon might bar]=]},
     {GW.Enum.ChangelogType.bug, [=[Fix party unit values]=]},
